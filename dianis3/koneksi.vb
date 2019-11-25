fc8e0b86b4a8e823464435e9cde04e526af346be
Taufan <taufanderazor@gmail.com>
2019-11-25 20:39:58 +0700

    pindah lokasi bioskopkita

---
+++ dianis3/koneksi.vb
@@ -0,0 +1,19 @@
+﻿
+Imports System.Data.OleDb
+Module koneksi
+    Public Conn As OleDbConnection
+    Public DA As OleDbDataAdapter
+    Public DS As DataSet
+    Public CMD As OleDbCommand
+    Public DM As OleDbDataReader
+
+    Sub koneksiDB()
+        Try
+            Conn = New OleDbConnection("provider=microsoft.ace.oledb.12.0; data source = bioskopkita.accdb")
+            Conn.Open()
+            MsgBox("Mantap pan")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
