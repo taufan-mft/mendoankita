a9a680cc71876942d47a7b9ae9661f057dedc0d4
yogiprnomo <57977030+yogiprnomo@users.noreply.github.com>
2020-01-05 15:05:31 +0700

    yeay report bisa di cr viewer wkwk
    ayo pan commit wkwkwk

--- dianis3/CrViewer.Designer.vb
+++ dianis3/CrViewer.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class CRViewer
+Partial Class crviewer
     Inherits MetroFramework.Forms.MetroForm
 
     'Form overrides dispose to clean up the component list.
@@ -23,30 +23,44 @@ Partial Class CRViewer
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.CrystalReportPegawai1 = New dianis3.CrystalReportPegawai()
+        Me.CrystalReportAudio1 = New dianis3.CrystalReportAudio()
+        Me.CrystalReportFilm1 = New dianis3.CrystalReportFilm()
+        Me.CrystalReportFnb1 = New dianis3.CrystalReportFnb()
+        Me.CrystalReportTiket1 = New dianis3.CrystalReportTiket()
+        Me.CrystalReportPegawai2 = New dianis3.CrystalReportPegawai()
         Me.SuspendLayout()
         '
         'CrystalReportViewer1
         '
-        Me.CrystalReportViewer1.ActiveViewIndex = -1
+        Me.CrystalReportViewer1.ActiveViewIndex = 0
         Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
         Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.CrystalReportViewer1.Location = New System.Drawing.Point(20, 60)
         Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.ReportSource = Me.CrystalReportPegawai2
         Me.CrystalReportViewer1.Size = New System.Drawing.Size(760, 370)
-        Me.CrystalReportViewer1.TabIndex = 0
+        Me.CrystalReportViewer1.TabIndex = 1
+        Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
         '
-        'CRViewer
+        'crviewer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.CrystalReportViewer1)
-        Me.Name = "CRViewer"
-        Me.Text = "CrViewer"
+        Me.Name = "crviewer"
+        Me.Text = "crviewer"
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents CrystalReportPegawai1 As CrystalReportPegawai
+    Friend WithEvents CrystalReportAudio1 As CrystalReportAudio
+    Friend WithEvents CrystalReportFilm1 As CrystalReportFilm
+    Friend WithEvents CrystalReportTiket1 As CrystalReportTiket
+    Friend WithEvents CrystalReportFnb1 As CrystalReportFnb
+    Friend WithEvents CrystalReportPegawai2 As CrystalReportPegawai
 End Class
--- dianis3/CrViewer.vb
+++ dianis3/CrViewer.vb
@@ -1,3 +1,5 @@
-﻿Public Class CRViewer
+﻿Public Class crviewer
+    Private Sub CrystalReportViewer1_Load(sender As Object, e As EventArgs) Handles CrystalReportViewer1.Load
 
+    End Sub
 End Class
